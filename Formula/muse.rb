1d6d0ea38bfc181592c7841bb987c118db2658c4
Stefano Zampieri <szampier@eso.org>
2023-07-03 17:51:34 +0200

    add muse

---
+++ Formula/muse.rb
@@ -0,0 +1,29 @@
+class Muse < Formula
+  desc "ESO MUSE recipe plugins"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.8.9-1.tar.gz"
+  sha256 "2884204f395813131ac11af63a19c03ec8747051286392c39d03f2d11bc94917"
+  license "GPL-2.0-or-later"
+
+  depends_on "cpl"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "gsl"
+  depends_on "pkg-config"
+
+  def install
+    system "tar", "xf", "muse-2.8.9.tar.gz"
+    cd "muse-2.8.9" do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_equal "aaa", "aaa"
+  end
+end
