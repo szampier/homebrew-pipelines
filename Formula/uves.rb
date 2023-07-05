0068a2d22876f3c018c235e8bfc71066c544bad9
szampier <56970291+szampier@users.noreply.github.com>
2023-07-05 10:11:28 +0200

    build from kit, add livecheck and test (#17)

--- Formula/uves.rb
+++ Formula/uves.rb
@@ -1,12 +1,16 @@
 class Uves < Formula
   desc "ESO UVES recipe plugins"
-  homepage "https://www.eso.org/public/"
-  url "https://github.com/szampier/homebrew-pipelines/blob/main/src/uves-6.3.1.tar.gz?raw=true"
-  sha256 "8a7086ab32d0ae7a134664c8f4c21b5dd48cf27fba69cd47be006b35e92efca7"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.1.12-1.tar.gz"
+  sha256 "9d77e6ac8e37110cf2f5289acebb682bbc583e1274e78f0a0b3f2c61bf1aec1c"
   license "GPL-2.0-or-later"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?uves-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
   bottle do
-    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/uves-6.3.1.tar"
     sha256 cellar: :any,                 ventura:      "30a18fc0da532a36d2246c49fe86dbfde8e8a51ae1849970ba70b7588ea231e0"
     sha256 cellar: :any,                 monterey:     "7150f8de6020c63aaf246824925c4a6e8d32df11d7dbb9b417bb8ddb4554b0c3"
     sha256 cellar: :any,                 big_sur:      "8a168bb3fa40cf3bf0cd2c91d4e0cc50f46d44d5a0d730e4f57d2f7478fce321"
@@ -16,25 +20,26 @@ class Uves < Formula
   depends_on "cpl"
   depends_on "curl"
   depends_on "erfa"
+  depends_on "esorex"
   depends_on "gsl"
   depends_on "pkg-config"
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
-
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-cpl=#{Formula["cpl"].prefix}",
-                          "--with-gsl=#{Formula["gsl"].prefix}",
-                          "--with-erfa=#{Formula["erfa"].prefix}",
-                          "--with-curl=#{Formula["curl"].prefix}"
-    system "make", "install"
+    system "tar", "xf", "uves-6.1.12.tar.gz"
+    cd "uves-6.1.12" do
+      # Fix -flat_namespace being used on Big Sur and later.
+      system "curl", "-O", "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      system "patch", "configure", "configure-big_sur.diff"
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
   end
 
   test do
-    assert_equal "aaa", "aaa"
+    system "esorex", "--man-page", "uves_cal_mbias"
   end
 end
