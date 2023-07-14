c1103e6f51ef23385dcd6735bebd6abf8299efae
Stefano Zampieri <szampier@eso.org>
2023-07-14 16:47:39 +0200

    remove common/src, edit startup script

--- Formula/esoreflex.rb
+++ Formula/esoreflex.rb
@@ -6,10 +6,17 @@ class Esoreflex < Formula
   license "GPL-2.0-or-later"
 
   depends_on "esorex"
+  depends_on "java11"
 
   def install
     pkgshare.install Dir["*"]
-    bin.install_symlink (pkgshare/"esoreflex/bin").children
+    bin.install_symlink pkgshare/"esoreflex/bin/esoreflex"
+    inreplace pkgshare/"esoreflex/bin/esoreflex" do |s|
+      s.gsub! "$(dirname $0)/../..", HOMEBREW_PREFIX/"share/esoreflex"
+      s.gsub! "~/KeplerData/workflows/MyWorkflows",
+HOMEBREW_PREFIX/"share/reflex/workflows:~/KeplerData/workflows/MyWorkflows"
+    end
+    rm_rf pkgshare/"common/src"
   end
 
   test do
