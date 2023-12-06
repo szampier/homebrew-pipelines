16a7733e9b99ac0cefa29219a6b0bd353061c431
Stefano Zampieri <szampier@eso.org>
2023-12-06 23:40:34 +0100

    uves 6.4.1

--- Formula/uves.rb
+++ Formula/uves.rb
@@ -1,8 +1,8 @@
 class Uves < Formula
   desc "ESO UVES recipe plugins"
   homepage "https://www.eso.org/sci/software/pipelines/"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.1.12-1.tar.gz"
-  sha256 "9d77e6ac8e37110cf2f5289acebb682bbc583e1274e78f0a0b3f2c61bf1aec1c"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.1.tar.gz"
+  sha256 "6914c994aa9170d5b2a05622f8beb8c3ef0014e18cd0408530765b7ec321a142"
   license "GPL-2.0-or-later"
 
   livecheck do
