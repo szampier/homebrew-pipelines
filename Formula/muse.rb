324a84b4e03d2548d3270d39375c28ec472e4ad7
Stefano Zampieri <szampier@eso.org>
2024-03-21 17:13:17 +0100

    muse 2.8.9-2
    Closes #29.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/muse.rb
+++ Formula/muse.rb
@@ -1,8 +1,8 @@
 class Muse < Formula
   desc "ESO MUSE recipe plugins"
   homepage "https://www.eso.org/sci/software/pipelines/"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.8.9-1.tar.gz"
-  sha256 "2884204f395813131ac11af63a19c03ec8747051286392c39d03f2d11bc94917"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.8.9-2.tar.gz"
+  sha256 "2a95fe6b90b9829218f295d2d73daa14691d00618679d904f83fac25f8e5948d"
   license "GPL-2.0-or-later"
 
   livecheck do
