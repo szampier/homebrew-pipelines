0669c5357a7351643bff5277b3dfa846061405fc
Stefano Zampieri <szampier@eso.org>
2024-03-21 14:39:22 +0100

    update sha256
    Closes #26.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/erfa.rb
+++ Formula/erfa.rb
@@ -2,7 +2,7 @@ class Erfa < Formula
   desc "Essential Routines for Fundamental Astronomy"
   homepage "https://www.eso.org/sci/software/cpl/"
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/erfa/erfa-2.0.1.tar.gz"
-  sha256 "75cb0a2cc1561d24203d9d0e67c21f105e45a70181d57f158e64a46a50ccd515"
+  sha256 "3aae5f93abcd1e9519a4a0a5d6c5c1b70f0b36ca2a15ae4589c5e594f3d8f1c0"
   license "GPL-2.0-or-later"
 
   bottle do
