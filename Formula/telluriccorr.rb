0a2c03eab1d122c567d1de499076d5481c2bbc4f
Stefano Zampieri <szampier@eso.org>
2024-03-22 07:59:32 +0100

    revision 1

--- Formula/telluriccorr.rb
+++ Formula/telluriccorr.rb
@@ -4,6 +4,7 @@ class Telluriccorr < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/telluriccorr/telluriccorr-4.3.1.tar.gz"
   sha256 "a02dc7389588033efd22f71f0712f1b97d00cd732f701dee1b1e093dc062a64b"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     root_url "https://github.com/szampier/homebrew-pipelines/releases/download/telluriccorr-4.3.1"
