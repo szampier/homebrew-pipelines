c375a52837988c42b8ef26c06c5271928825650f
Stefano Zampieri <szampier@eso.org>
2024-03-22 09:52:43 +0100

    add macos-14, rebuild cext erfa esorex

--- Formula/erfa.rb
+++ Formula/erfa.rb
@@ -4,6 +4,7 @@ class Erfa < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/erfa/erfa-2.0.1.tar.gz"
   sha256 "3aae5f93abcd1e9519a4a0a5d6c5c1b70f0b36ca2a15ae4589c5e594f3d8f1c0"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     root_url "https://github.com/szampier/homebrew-pipelines/releases/download/erfa-2.0.1"
