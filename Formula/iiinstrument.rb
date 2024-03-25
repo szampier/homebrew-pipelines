e9759774ab3a1d2dedf64cadaf4e39ba6d31d822
Stefano Zampieri <szampier@eso.org>
2024-03-25 18:23:52 -0400

    extract version

--- Formula/iiinstrument.rb
+++ Formula/iiinstrument.rb
@@ -2,6 +2,7 @@ class Iiinstrument < Formula
   desc "ESO example template instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/iiinstrument/iiinstrument-kit-0.1.14-27.tar.gz"
+  version "0.1.14"
   sha256 "8b281fab3de19c38dfac76e43c3f40b64be26e884d5883afbd051018b8983a9a"
   license "GPL-2.0-or-later"
 
@@ -24,7 +25,6 @@ class Iiinstrument < Formula
   depends_on "gsl"
 
   def install
-    # version = url[/.*-kit-(\d+(?:[.]\d+)+)/i, 1]
     system "tar", "xf", "iiinstrument-#{version}.tar.gz"
     cd "iiinstrument-#{version}" do
       # Fix -flat_namespace being used on Big Sur and later.
