f90b8f43e50f03c99bf3910618c85d6629e1a8d1
szampier <56970291+szampier@users.noreply.github.com>
2024-04-01 16:16:45 +0000

    uves-calib: add 6.4.1-1 bottle.

--- Formula/uves-calib.rb
+++ Formula/uves-calib.rb
@@ -10,6 +10,15 @@ class UvesCalib < Formula
     regex(/href=.*?uves-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/uves-calib-6.4.1-1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43e4e25aa5c6b3e47ddbb8c2a1939d82893519cf9b6d53877674839e7b7fd00e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1ba8b68cb559f1c62cd436ae330e3d490690cef3a155b559fa1773482766b805"
+    sha256 cellar: :any_skip_relocation, ventura:       "0f7710b7cd281efaca4dcae647e49d9d30a7d2199676ea3e94ce915349c0aff4"
+    sha256 cellar: :any_skip_relocation, monterey:      "7e9f86cf3d948e4717152c62b533f0c1d2af8e6fcf829f4eeb496e2cbc4f9a0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d618621b4ec90b776d16246ecea0bc8c4a63280547b4cc0b71dae8cd4278223f"
+  end
+
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
     system "tar", "xf", "uves-calib-#{version_norevision}.tar.gz"
