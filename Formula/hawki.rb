1e87608831ad93d263ad5c2f73acbf1fdfdfae41
Stefano Zampieri <szampier@eso.org>
2024-04-05 18:28:05 -0400

    hawki 2.4.13

---
+++ Formula/hawki.rb
@@ -0,0 +1,33 @@
+class Hawki < Formula
+  desc "ESO HAWKI recipe plugins"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/hawki/hawki-kit-2.4.13-3.tar.gz"
+  sha256 "a48297e56d216d566fb1bbb112d85243ba2a013c5ef9302c9dbe8745038d42e3"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?hawki-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cpl"
+  depends_on "esorex"
+  depends_on "gsl"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "hawki-#{version_norevision}.tar.gz"
+    cd "hawki-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "hawki_cal_dark -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page hawki_cal_dark")
+  end
+end
