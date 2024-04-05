478520319fca7995324a65115e9528ed4e67548d
szampier <56970291+szampier@users.noreply.github.com>
2024-04-05 22:40:55 +0000

    hawki-calib: add 2.4.13-3 bottle.

--- Formula/hawki-calib.rb
+++ Formula/hawki-calib.rb
@@ -10,6 +10,15 @@ class HawkiCalib < Formula
     regex(/href=.*?hawki-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/hawki-calib-2.4.13-3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fd7c6b91b52d1ad3e365969bc24bc8bdb700e8b082782883745ab9a7be39e1bf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a65423d740508bd4edcfa5ca16aba3e0997ea542627d4677bc0f171f1ad60969"
+    sha256 cellar: :any_skip_relocation, ventura:       "3aa5ba289b780597b5480a484c7ba7332eac21fc2d3ccbb14fa527e92424d270"
+    sha256 cellar: :any_skip_relocation, monterey:      "9a5147dca8cb626886eb356e872f38e1ae2148579e633b0a78a2cd201d5c7ca3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8eaf9d05e9dfabede04f97580ad1c3842cb2a36656fe95a123999114861dbcac"
+  end
+
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
     system "tar", "xf", "hawki-calib-#{version_norevision}.tar.gz"
