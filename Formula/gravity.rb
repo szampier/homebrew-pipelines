86ec7e9723a7eb52830033ef49097c9a27ef1b03
Stefano Zampieri <szampier@eso.org>
2024-05-04 16:15:09 +0200

    Add gravity

---
+++ Formula/gravity.rb
@@ -0,0 +1,40 @@
+class Gravity < Formula
+  desc "ESO GRAVITY recipe plugins"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.6.6.tar.gz"
+  sha256 "300be5f0b9907439485ee5c7aad784d93a1a679167e365a0eb3cd61ebc27b24d"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?gravity-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cpl"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "gravity-#{version_norevision}.tar.gz"
+    cd "gravity-#{version_norevision}" do
+      # Fix -flat_namespace being used on Big Sur and later.
+      # system "curl", "-O", "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      # system "patch", "configure", "configure-big_sur.diff"
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "gravity_dark -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page gravity_dark")
+  end
+end
