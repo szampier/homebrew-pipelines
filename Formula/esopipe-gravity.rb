fc68ffa40ab8b2531ed29292a030a2c4e5694d7c
szampier <56970291+szampier@users.noreply.github.com>
2024-05-04 14:37:39 +0000

    esopipe-gravity: add 1.6.6 bottle.

--- Formula/esopipe-gravity.rb
+++ Formula/esopipe-gravity.rb
@@ -10,6 +10,15 @@ class EsopipeGravity < Formula
     regex(/href=.*?gravity-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/esopipe-gravity-1.6.6"
+    sha256 cellar: :any,                 arm64_sonoma:  "b0707c3b8d47f3bff3abb3bc47a8ba832dc1ad6076746556ab1a030dcf08baca"
+    sha256 cellar: :any,                 arm64_ventura: "11023d467d29bb046ad75fc8700c523c5abe6437e1bf5f3c331aa27b08700ac5"
+    sha256 cellar: :any,                 ventura:       "927500327b73d97ac4d53190a3b47a14933f6d444d822fb725a7862f7d51afa0"
+    sha256 cellar: :any,                 monterey:      "1de258ed99d3d661398b1b09895a3d76b5027afec8820c719a293923346b48bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a232ae9b18aec9944ef95ad39357ec8a06b83435c4c3399e281290546dc9c549"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cpl"
   depends_on "curl"
