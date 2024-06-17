62bc14e0abc6e02d2f217115dd2c99129952987b
Stefano Zampieri <szampier@eso.org>
2024-06-17 18:30:43 +0200

    uves-calib 6.4.6
    Closes #67.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/uves-calib.rb
+++ Formula/uves-calib.rb
@@ -1,8 +1,8 @@
 class UvesCalib < Formula
   desc "ESO UVES instrument pipeline (calibration data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.1-1.tar.gz"
-  sha256 "b801adb7ad804fe8b097fd29caaf04ebd3d61beaaabb902e2fa37330f471a4b1"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.6.tar.gz"
+  sha256 "0d80c8cd55a271f2cb31549483ee139ac2e8054c759825d56bb605c41779a10a"
   license "GPL-2.0-or-later"
 
   livecheck do
