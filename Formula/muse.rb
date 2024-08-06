65657df0b2bdf638e285757e72b24b4fe4790d8a
szampier <56970291+szampier@users.noreply.github.com>
2024-08-06 15:40:22 +0000

    muse: update 2.10.10 bottle.

--- Formula/muse.rb
+++ Formula/muse.rb
@@ -11,12 +11,12 @@ class Muse < Formula
   end
 
   bottle do
-    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/muse-2.8.9-2_1"
-    sha256 cellar: :any,                 arm64_sonoma:  "13f29fad481f7a4c1e58b28046b84a676d25c5f0e1aff7d563f2726380a3af96"
-    sha256 cellar: :any,                 arm64_ventura: "471938f8a0beeb7c1c6dcb0f7d9c75912752e267010094bee19f436778d9444d"
-    sha256 cellar: :any,                 ventura:       "b190a0ff16636af22db756b563d05a6743f255e8b9482029f2499569c2673da9"
-    sha256 cellar: :any,                 monterey:      "2a1f4be7ebe1ebcba558d3834e273be112831b7a3ba7e81450b3ac96b3871d47"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f905ceea03d596b3b540e11ae7e8c8aeeb1cac76dc38e8a4c9cec934c0149f97"
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/muse-2.10.10"
+    sha256 cellar: :any,                 arm64_sonoma:  "ddf8b661680a66e00d6a23a308e7c05a3c9804e9ae1c17bf4d4744aef8660f82"
+    sha256 cellar: :any,                 arm64_ventura: "429fd2d0346949c8ea8645d0fbba9d9a78c3bca684cbc8236d428de4e5a6ddb6"
+    sha256 cellar: :any,                 ventura:       "ad1488b38c7be4bbf95e1cb2e1016028aeeb155692ab3b4e8fe9d7338ed318a6"
+    sha256 cellar: :any,                 monterey:      "ee30f7f41193c0dcb068c0df75d8537a3eba7d5940d836224863f5885f9d01e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de1e0cfebf0ccfd7a6fc3849712db5605e9ee52909eb2ab93ca1878e7aa35938"
   end
 
   depends_on "cpl"
