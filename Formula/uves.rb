9a84df119c8038b22badeb154c145e201fd7d232
szampier <56970291+szampier@users.noreply.github.com>
2024-08-07 11:16:18 +0200

    Update uves.rb

--- Formula/uves.rb
+++ Formula/uves.rb
@@ -4,6 +4,7 @@ class Uves < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.6.tar.gz"
   sha256 "0d80c8cd55a271f2cb31549483ee139ac2e8054c759825d56bb605c41779a10a"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
