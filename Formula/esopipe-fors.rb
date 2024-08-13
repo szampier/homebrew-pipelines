4beae1133740f2fe3745a73d7405a45fa71d9cba
szampier <56970291+szampier@users.noreply.github.com>
2024-08-13 15:06:34 +0000

    esopipe-fors: add 5.6.5-7 bottle.

--- Formula/esopipe-fors.rb
+++ Formula/esopipe-fors.rb
@@ -14,6 +14,15 @@ class EsopipeFors < Formula
     regex(/href=.*?fors-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/esopipe-fors-5.6.5-7"
+    sha256 arm64_sonoma:  "791898ddbbc127b7a2bb068d82f276709ce1b79fc7eb96e85c5a0f8cad5323f3"
+    sha256 arm64_ventura: "8eaff368b76813eda0ab356da7be8f23db21ed071070353f6a648be472902db1"
+    sha256 ventura:       "d9c28e2d8f029840fe3eaa4135b536ebc8f620dd7023089f5b4391692286f4fd"
+    sha256 monterey:      "b3f6b1a52cda5c73e74f90690ba0a6cc8651a3290f8da57cb7bfff61e46cdbcb"
+    sha256 x86_64_linux:  "75e520ba75a9482c558b2eaedd8af2ec06d105a48a012a72cefe7b7e388f3142"
+  end
+
   depends_on "cfitsio"
   depends_on "cpl"
   depends_on "curl"
