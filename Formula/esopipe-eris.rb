1fa8f63ab0adb82fea00682e2b024ab248089e82
Stefano Zampieri <szampier@eso.org>
2024-08-23 18:53:44 +0200

    add eris

---
+++ Formula/esopipe-eris.rb
@@ -0,0 +1,39 @@
+class EsopipeEris < Formula
+  desc "ESO ERIS recipe plugins"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/eris/eris-kit-1.6.0.tar.gz"
+  sha256 "00c5947cb3e7d7390a10b2c9e7865ce33c1aeb10f7562c01f6748f35c52b5822"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?eris-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cfitsio"
+  depends_on "cpl"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "eris-#{version_norevision}.tar.gz"
+    cd "eris-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+             "--with-cfitsio=#{Formula["cfitsio"].prefix}",
+             "--with-cpl=#{Formula["cpl"].prefix}",
+             "--with-erfa=#{Formula["erfa"].prefix}",
+             "--with-curl=#{Formula["curl"].prefix}",
+             "--with-gsl=#{Formula["gsl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "xsh_mdark -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page xsh_mdark")
+  end
+end
