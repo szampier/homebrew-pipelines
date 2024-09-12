83459ffa61cd0c2e2440af679402ca1251a1352d
Stefano Zampieri <szampier@eso.org>
2024-09-12 18:07:53 +0200

    add cr2res

---
+++ Formula/esopipe-cr2res.rb
@@ -0,0 +1,39 @@
+class EsopipeCr2res < Formula
+  desc "ESO CR2RES recipe plugins"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/cr2res/cr2re-kit-1.4.4.tar.gz"
+  sha256 "bd4a4d1025ea98ed5372680f877c3b20521109600436c5621e876e994f6f6152"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?cr2re-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cfitsio"
+  depends_on "cpl"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "cr2re-#{version_norevision}.tar.gz"
+    cd "cr2re-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+             "--with-cfitsio=#{Formula["cfitsio"].prefix}",
+             "--with-cpl=#{Formula["cpl"].prefix}",
+             "--with-erfa=#{Formula["erfa"].prefix}",
+             "--with-curl=#{Formula["curl"].prefix}",
+             "--with-gsl=#{Formula["gsl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "gimasterbias -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page gimasterbias")
+  end
+end
