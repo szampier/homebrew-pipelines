40d3ffa2b86417bed610a1be81479421e30f27da
Stefano Zampieri <szampier@eso.org>
2024-09-12 21:27:58 +0200

    hawki 2.5.8
    Closes #85.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/hawki.rb
+++ Formula/hawki.rb
@@ -1,8 +1,8 @@
 class Hawki < Formula
   desc "ESO HAWKI recipe plugins"
   homepage "https://www.eso.org/sci/software/pipelines/"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/hawki/hawki-kit-2.5.7.tar.gz"
-  sha256 "c0c6920b1f098f63a8d33c50865bde75ddc0c6fd5c986acda226304f6041f13b"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/hawki/hawki-kit-2.5.8.tar.gz"
+  sha256 "8c5640b1ea05d790ab708169c303fa43a143002b295a3b870c4300d49cd6ff5c"
   license "GPL-2.0-or-later"
 
   livecheck do
