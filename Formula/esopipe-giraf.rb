812b9c1531b3f47d958cd1f914ed6ad195df5cb9
Stefano Zampieri <szampier@eso.org>
2024-09-12 16:16:09 +0200

    add giraffe
    Closes #83.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

---
+++ Formula/esopipe-giraf.rb
@@ -0,0 +1,33 @@
+class EsopipeGiraf < Formula
+  desc "ESO GIRAFFE recipe plugins"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/giraffe/giraf-kit-2.16.12.tar.gz"
+  sha256 "6920f5a672fd2f8bb312180e922707707e747661e6767897bb229afa4da89a04"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?giraf-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cfitsio"
+  depends_on "cpl"
+  depends_on "esorex"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "giraf-#{version_norevision}.tar.gz"
+    cd "giraf-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+             "--with-cfitsio=#{Formula["cfitsio"].prefix}",
+             "--with-cpl=#{Formula["cpl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "gimasterbias -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page gimasterbias")
+  end
+end
