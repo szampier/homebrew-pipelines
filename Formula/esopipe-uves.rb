321de87554fcc4a13c7512182949281c63198c5e
Stefano Zampieri <szampier@eso.org>
2024-10-09 15:00:38 +0200

    rename to esopipe-uves and make it dependent on recipes

---
+++ Formula/esopipe-uves.rb
@@ -0,0 +1,24 @@
+class EsopipeUves < Formula
+  desc "ESO UVES instrument pipeline (static data)"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.6.tar.gz"
+  sha256 "0d80c8cd55a271f2cb31549483ee139ac2e8054c759825d56bb605c41779a10a"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?uves-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "esopipe-uves-recipes"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "uves-calib-#{version_norevision}.tar.gz"
+    (prefix/"share/esopipes/datastatic/uves-#{version_norevision}").install Dir["uves-calib-#{version_norevision}/cal/*"]
+  end
+
+  test do
+    system "true"
+  end
+end
