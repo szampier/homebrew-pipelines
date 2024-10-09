6f3f5f4f62d580df7e0da221b03e71bd597274a1
Stefano Zampieri <szampier@eso.org>
2024-10-09 22:03:01 +0200

    rename hawki

--- Formula/esopipe-hawki-recipes.rb
+++ Formula/esopipe-hawki-recipes.rb
@@ -1,5 +1,5 @@
-class Hawki < Formula
-  desc "ESO HAWKI recipe plugins"
+class EsopipeHawkiRecipes < Formula
+  desc "ESO HAWKI instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/hawki/hawki-kit-2.5.8.tar.gz"
   sha256 "8c5640b1ea05d790ab708169c303fa43a143002b295a3b870c4300d49cd6ff5c"
