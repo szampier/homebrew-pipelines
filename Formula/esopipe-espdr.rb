f861450e057f2ff164a90126c6e307b35c398792
Stefano Zampieri <szampier@eso.org>
2024-10-09 21:55:29 +0200

    rename espdr-calib and make it dependent on recipes

--- Formula/esopipe-espdr.rb
+++ Formula/esopipe-espdr.rb
@@ -1,5 +1,5 @@
-class EspdrCalib < Formula
-  desc "ESO ESPRESSO instrument pipeline (calibration data)"
+class EsopipeEspdr < Formula
+  desc "ESO ESPRESSO instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso/espdr-kit-3.2.0.tar.gz"
   sha256 "8d7d04a8434684a5e941e4d33a77eba5ce26e9c195e0d357c1ebc94944cf5a7a"
@@ -9,6 +9,8 @@ class EspdrCalib < Formula
     url :homepage
     regex(/href=.*?espdr-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
+
+  depends_on "esopipe-espdr-recipes"
 
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
