9a48f134a80eb80ddd003d3dcce40b42d264887b
Stefano Zampieri <szampier@eso.org>
2024-10-09 22:47:33 +0200

    rename hawki-calib.rb and make it dependent on recipes

--- Formula/esopipe-hawki.rb
+++ Formula/esopipe-hawki.rb
@@ -1,5 +1,5 @@
-class HawkiCalib < Formula
-  desc "ESO HAWKI instrument pipeline (calibration data)"
+class EsopipeHawki < Formula
+  desc "ESO HAWKI instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/hawki/hawki-kit-2.5.8.tar.gz"
   sha256 "8c5640b1ea05d790ab708169c303fa43a143002b295a3b870c4300d49cd6ff5c"
@@ -9,6 +9,8 @@ class HawkiCalib < Formula
     url :homepage
     regex(/href=.*?hawki-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
+
+  depends_on "esopipe-hawki-recipes"
 
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
