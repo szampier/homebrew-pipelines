60feed31792da55df823c91bf54650cd68882065
Stefano Zampieri <szampier@eso.org>
2024-10-10 10:28:27 +0200

    rename esopipe-kmos-datastatic.rb and make it dependent on recipes

--- Formula/esopipe-kmos.rb
+++ Formula/esopipe-kmos.rb
@@ -1,5 +1,5 @@
-class EsopipeKmosDatastatic < Formula
-  desc "ESO KMOS instrument pipeline (calibration data)"
+class EsopipeKmos < Formula
+  desc "ESO KMOS instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/kmos/kmos-kit-4.4.4.tar.gz"
   sha256 "455b58e777335f59f31e7af56d77d64f54ea8a9f58dedae9851fbf75864de612"
@@ -9,6 +9,8 @@ class EsopipeKmosDatastatic < Formula
     url :homepage
     regex(/href=.*?kmos-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
+
+  depends_on "esopipe-kmos-recipes"
 
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
