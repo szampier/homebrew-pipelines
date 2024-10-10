0f38a0432431b38727ee8e789bd400fbad581d2c
Stefano Zampieri <szampier@eso.org>
2024-10-10 10:00:57 +0200

    rename esopipe-muse-datastatic.rb to esopipe-muse.rb

--- Formula/esopipe-muse.rb
+++ Formula/esopipe-muse.rb
@@ -1,5 +1,5 @@
-class EsopipeMuseDatastatic < Formula
-  desc "ESO MUSE instrument pipeline (calibration data)"
+class EsopipeMuse < Formula
+  desc "ESO MUSE instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.10.10.tar.gz"
   sha256 "d3d51e755a0fdffc21d5730e0041f4e3a4826ec9ffcb97c1f0f69860a7a693df"
@@ -9,6 +9,8 @@ class EsopipeMuseDatastatic < Formula
     url :homepage
     regex(/href=.*?muse-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
+
+  depends_on "esopipe-muse-recipes"
 
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
