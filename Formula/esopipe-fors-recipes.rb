0d145051e8e23d6f01e83ad06bb2d1852fd28d8e
Stefano Zampieri <szampier@eso.org>
2024-10-10 10:40:30 +0200

    rename esopipe-fors.rb to esopipe-fors-recipes.rb

--- Formula/esopipe-fors-recipes.rb
+++ Formula/esopipe-fors-recipes.rb
@@ -1,9 +1,5 @@
-# typed: strict
-# frozen_string_literal: true
-
-# Fors
-class EsopipeFors < Formula
-  desc "ESO FORS recipe plugins"
+class EsopipeForsRecipes < Formula
+  desc "ESO FORS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/fors/fors-kit-5.6.5-7.tar.gz"
   sha256 "e492be42ae3b96e48a2a3b2981feff8712fb2d616fd1f3f3f42ba243add2a15b"
