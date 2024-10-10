bb542d1a0da9f491e1f59d1d363bd235b613cafe
Stefano Zampieri <szampier@eso.org>
2024-10-10 11:41:17 +0200

    rename esopipe-fors-datastatic.rb and make it dependent on recipes

--- Formula/esopipe-fors.rb
+++ Formula/esopipe-fors.rb
@@ -1,5 +1,5 @@
-class EsopipeForsDatastatic < Formula
-  desc "ESO FORS instrument pipeline (calibration data)"
+class EsopipeFors < Formula
+  desc "ESO FORS instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/fors/fors-kit-5.6.5-7.tar.gz"
   sha256 "e492be42ae3b96e48a2a3b2981feff8712fb2d616fd1f3f3f42ba243add2a15b"
@@ -9,6 +9,8 @@ class EsopipeForsDatastatic < Formula
     url :homepage
     regex(/href=.*?fors-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
+
+  depends_on "esopipe-fors-recipes"
 
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
