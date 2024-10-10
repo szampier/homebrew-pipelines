e633b87c217acf6306bd591bd791de1af47aca09
Stefano Zampieri <szampier@eso.org>
2024-10-10 08:36:53 +0200

    rename muse recipes
    Closes #91.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-muse-recipes.rb
+++ Formula/esopipe-muse-recipes.rb
@@ -1,5 +1,5 @@
-class Muse < Formula
-  desc "ESO MUSE recipe plugins"
+class EsopipeMuseRecipes < Formula
+  desc "ESO MUSE instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.10.10.tar.gz"
   sha256 "d3d51e755a0fdffc21d5730e0041f4e3a4826ec9ffcb97c1f0f69860a7a693df"
