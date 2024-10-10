11f980360561799d058b56760fe93b37527a5a8f
Stefano Zampieri <szampier@eso.org>
2024-10-10 12:06:51 +0200

    rename esopipe-xshooter-datastatic.rb and make it dependent on recipes

--- Formula/esopipe-xshoo.rb
+++ Formula/esopipe-xshoo.rb
@@ -1,9 +1,5 @@
-# typed: strict
-# frozen_string_literal: true
-
-## Datastatic
-class EsopipeXshooterDatastatic < Formula
-  desc "ESO XSHOOTER recipe plugins (Calibration Data)"
+class EsopipeXshoo < Formula
+  desc "ESO XSHOOTER instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/xshooter/xshoo-kit-3.6.8.tar.gz"
   sha256 "e50f7172d65de8b8f54fc47a333d49a5fa6db34aabe097bbc1a3449f455ab2b9"
@@ -13,6 +9,8 @@ class EsopipeXshooterDatastatic < Formula
     url :homepage
     regex(/href=.*?xshoo-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
+
+  depends_on "esopipe-xshoo-recipes"
 
   def install
     version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
