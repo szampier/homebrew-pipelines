66f854a16ea7700e9bc80a95c132b3e770f0d044
Stefano Zampieri <szampier@eso.org>
2024-10-11 15:55:43 +0200

    Add detmon recipes
    Closes #96.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

---
+++ Formula/esopipe-detmon-recipes.rb
@@ -0,0 +1,37 @@
+class EsopipeDetmonRecipes < Formula
+  desc "ESO DETMON instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/detmon/detmon-kit-1.3.14.tar.gz"
+  sha256 "4d7ea0eb8e082d741ebd074c53165d2b7b1868582bde57ab715833efd17f69f3"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?detmon-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cpl"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "detmon-#{version_norevision}.tar.gz"
+    cd "detmon-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl"].prefix}",
+                            "--with-gsl=#{Formula["gsl"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "detmon_opt_lg -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page detmon_opt_lg")
+  end
+end
