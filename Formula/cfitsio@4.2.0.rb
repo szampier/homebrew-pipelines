a73916db9ce6da608478859a7d6ea1c20808389e
Stefano Zampieri <szampier@eso.org>
2024-10-15 09:50:32 +0200

    add cfitsio@4.2.0.rb
    Closes #99.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

---
+++ Formula/cfitsio@4.2.0.rb
@@ -0,0 +1,30 @@
+class CfitsioAT420 < Formula
+  desc "C access to FITS data files with optional Fortran wrappers"
+  homepage "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
+  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.2.0.tar.gz"
+  sha256 "eba53d1b3f6e345632bb09a7b752ec7ced3d63ec5153a848380f3880c5d61889"
+  license "CFITSIO"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?cfitsio[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  uses_from_macos "zlib"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--enable-reentrant"
+    system "make", "shared"
+    system "make", "install"
+    (pkgshare/"testprog").install Dir["testprog*"]
+  end
+
+  test do
+    cp Dir["#{pkgshare}/testprog/testprog*"], testpath
+    system ENV.cc, "testprog.c", "-o", "testprog", "-I#{include}",
+                   "-L#{lib}", "-lcfitsio"
+    system "./testprog > testprog.lis"
+    cmp "testprog.lis", "testprog.out"
+    cmp "testprog.fit", "testprog.std"
+  end
+end
