047d01144700e51759dbf7fa9a17fadcc8db359a
szampier <56970291+szampier@users.noreply.github.com>
2024-10-15 07:59:47 +0000

    cfitsio@4.2.0: add 4.2.0 bottle.

--- Formula/cfitsio@4.2.0.rb
+++ Formula/cfitsio@4.2.0.rb
@@ -10,6 +10,13 @@ class CfitsioAT420 < Formula
     regex(/href=.*?cfitsio[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/cfitsio@4.2.0-4.2.0"
+    sha256 cellar: :any,                 arm64_sonoma: "6cc4896c2e06522f32c28948956526d9a23660ad0545ec1123b64e67d6daee9f"
+    sha256 cellar: :any,                 ventura:      "de56e7ead31473d8ac02f8a498c9af50355fe6f0f39e2bdfe9388f73fd9666d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "52973416c34b3876b717809ec5bb3e9f816579916d9ecaa430e39b3eb44e0242"
+  end
+
   uses_from_macos "zlib"
 
   def install
