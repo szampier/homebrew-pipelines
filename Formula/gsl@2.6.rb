405d279edc810d7798f6d969a8a5523695116cd8
szampier <56970291+szampier@users.noreply.github.com>
2024-10-15 08:54:24 +0000

    gsl@2.6: add 2.6 bottle.

--- Formula/gsl@2.6.rb
+++ Formula/gsl@2.6.rb
@@ -6,6 +6,13 @@ class GslAT26 < Formula
   sha256 "b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/gsl@2.6-2.6"
+    sha256 cellar: :any,                 arm64_sonoma: "d0aa3c43f5d49006cd5052a0ad75fe59a9709f4599e048a47d2f6b488a269940"
+    sha256 cellar: :any,                 ventura:      "864516dfd6bbeac1718774344f37bd7fee8798ca68b56b8af68a559f62ee9297"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c48814999f65c1165bdfcb42e6cd561528a8f1ff39d19c9c09822073a919dfdb"
+  end
+
   def install
     ENV.deparallelize
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
