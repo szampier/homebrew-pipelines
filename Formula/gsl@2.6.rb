7222f6d62aaf93f0d5bb11d0697f3cde4f28dedf
Stefano Zampieri <szampier@eso.org>
2024-10-15 09:55:45 +0200

    add gsl@2.6.rb
    Closes #100.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

---
+++ Formula/gsl@2.6.rb
@@ -0,0 +1,19 @@
+class GslAT26 < Formula
+  desc "Numerical library for C and C++"
+  homepage "https://www.gnu.org/software/gsl/"
+  url "https://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz"
+  sha256 "b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8"
+  license "GPL-3.0-or-later"
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make" # A GNU tool which doesn't support just make install! Shameful!
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gsl-randist", "0", "20", "cauchy", "30"
+  end
+end
