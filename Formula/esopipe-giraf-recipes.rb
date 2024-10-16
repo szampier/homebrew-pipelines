91d1202f9568ad7aa50c1f862f1f0fe4bd185c08
Stefano Zampieri <szampier@eso.org>
2024-10-16 18:07:21 +0200

    esopipe-giraf-recipes: revision bump

--- Formula/esopipe-giraf-recipes.rb
+++ Formula/esopipe-giraf-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeGirafRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/giraffe/giraf-kit-2.16.12.tar.gz"
   sha256 "6920f5a672fd2f8bb312180e922707707e747661e6767897bb229afa4da89a04"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
