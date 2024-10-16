da755280fb719299f8b9657b78085f3d85e5dc9f
Stefano Zampieri <szampier@eso.org>
2024-10-16 13:40:18 +0200

    add esotk

---
+++ Formula/esopipe-esotk-recipes.rb
@@ -0,0 +1,38 @@
+class EsopipeEsotkRecipes < Formula
+  desc "ESO ESOTK instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-0.9.7-1.tar.gz"
+  sha256 "2d2db2d352b146b1f7c683cd1ee107bb5275e597ef412474ac4d39163fb6ce36"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?esotk-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl@2.6"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "esotk-#{version_norevision}.tar.gz"
+    cd "esotk-#{version_norevision}" do
+      system "./configure",
+             "--prefix=#{prefix}",
+             "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+             "--with-gsl=#{Formula["gsl@2.6"].prefix}",
+             "--with-erfa=#{Formula["erfa"].prefix}",
+             "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "esotk_opt_lg -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page esotk_opt_lg")
+  end
+end
