d984ec685ff9ab600e2d63ddc60877ca0ce89f02
Stefano Zampieri <szampier@eso.org>
2024-10-16 18:07:13 +0200

    esopipe-espdr-recipes: revision bump

--- Formula/esopipe-espdr-recipes.rb
+++ Formula/esopipe-espdr-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeEspdrRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso/espdr-kit-3.2.0.tar.gz"
   sha256 "8d7d04a8434684a5e941e4d33a77eba5ce26e9c195e0d357c1ebc94944cf5a7a"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
