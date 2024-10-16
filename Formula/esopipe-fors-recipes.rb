449b56f20b31dfbc879c522fafa49b4334b2d5fd
Stefano Zampieri <szampier@eso.org>
2024-10-16 18:07:17 +0200

    esopipe-fors-recipes: revision bump
    Closes #126.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-fors-recipes.rb
+++ Formula/esopipe-fors-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeForsRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/fors/fors-kit-5.6.5-7.tar.gz"
   sha256 "e492be42ae3b96e48a2a3b2981feff8712fb2d616fd1f3f3f42ba243add2a15b"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
