177d074bff60ade9a5c74020bd6861f5e6b08a9b
Stefano Zampieri <szampier@eso.org>
2024-10-16 18:07:34 +0200

    esopipe-kmos-recipes: revision bump
    Closes #119.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-kmos-recipes.rb
+++ Formula/esopipe-kmos-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeKmosRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/kmos/kmos-kit-4.4.4.tar.gz"
   sha256 "455b58e777335f59f31e7af56d77d64f54ea8a9f58dedae9851fbf75864de612"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
