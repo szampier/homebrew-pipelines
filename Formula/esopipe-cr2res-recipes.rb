3dfb36a7a63de412d683b02b7c94aaab30c1b312
Stefano Zampieri <szampier@eso.org>
2024-10-17 21:21:27 +0200

    update class name

--- Formula/esopipe-cr2res-recipes.rb
+++ Formula/esopipe-cr2res-recipes.rb
@@ -1,5 +1,5 @@
-class EsopipeCr2res < Formula
-  desc "ESO CR2RES recipe plugins"
+class EsopipeCr2resRecipes < Formula
+  desc "ESO CR2RES instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/cr2res/cr2re-kit-1.4.4.tar.gz"
   sha256 "bd4a4d1025ea98ed5372680f877c3b20521109600436c5621e876e994f6f6152"
