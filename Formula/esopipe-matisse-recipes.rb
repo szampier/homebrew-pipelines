65d7f6f7abd8dd9ea5a635664b2b37cbedcbb695
szampier <56970291+szampier@users.noreply.github.com>
2024-10-18 16:03:04 +0000

    esopipe-matisse-recipes: add 2.0.2-1 bottle.

--- Formula/esopipe-matisse-recipes.rb
+++ Formula/esopipe-matisse-recipes.rb
@@ -10,6 +10,13 @@ class EsopipeMatisseRecipes < Formula
     regex(/href=.*?matisse-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/esopipe-matisse-recipes-2.0.2-1"
+    sha256 cellar: :any,                 arm64_sonoma: "93ba7705486e15495ad91b6e4c00256219883f29db5cff11f47a47a62a932cac"
+    sha256 cellar: :any,                 ventura:      "33cbb04b9da67e98f862a03ee37838a55beb827988d83aa28cb97c33411ab1e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "023b86016105e505f28f4ddc161b70654e384998f68911c941d39aaf8f11def6"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cpl@7.3.2"
   depends_on "curl"
