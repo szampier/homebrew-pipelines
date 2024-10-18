59ebad511944d572aa018954a506e5ea0447e754
Stefano Zampieri <szampier@eso.org>
2024-10-18 16:38:45 +0200

    add nirps recipes

---
+++ Formula/esopipe-nirps-recipes.rb
@@ -0,0 +1,38 @@
+class EsopipeNirpsRecipes < Formula
+  desc "ESO NIRPS instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/nirps/nirps-kit-3.2.0.tar.gz"
+  sha256 "37bf774cb1b5f5bbe62c35e11b88d3ca592e97ee5a1d05091672b021a92e3530"
+  license "GPL-2.0-or-later"
+  revision 1
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?nirps-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl@2.6"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "nirps-#{version_norevision}.tar.gz"
+    cd "nirps-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                            "--with-gsl=#{Formula["gsl@2.6"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "espdr_mbias -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page espdr_mbias")
+  end
+end
