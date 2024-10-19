2ae9f4d7b85e0f68841a0ccc0b0800f2ae55788e
szampier <56970291+szampier@users.noreply.github.com>
2024-10-19 15:01:46 +0000

    esopipe-visir-recipes: add 4.4.5 bottle.

--- Formula/esopipe-visir-recipes.rb
+++ Formula/esopipe-visir-recipes.rb
@@ -10,6 +10,13 @@ class EsopipeVisirRecipes < Formula
     regex(/href=.*?visir-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/esopipe-visir-recipes-4.4.5"
+    sha256 arm64_sonoma: "e62773ca8d8af8a55fe0ca4906ddfd2e48016cb3ad8ca4aade6d04c88d84ebda"
+    sha256 ventura:      "241f7dde1aa3c32dad9f63f6df615b8a991b2a2dbe681e6665f43bf65294849f"
+    sha256 x86_64_linux: "30c60e8d4afbed591c149d26b6b68a6b756289a985c5304b93b3801306b61656"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cpl@7.3.2"
   depends_on "curl"
