99eba887847f101b7ed60e0fc35b888505a5c199
Stefano Zampieri <szampier@eso.org>
2024-10-19 16:30:23 +0200

    add sphere recipes

---
+++ Formula/esopipe-spher-recipes.rb
@@ -0,0 +1,43 @@
+class EsopipeSpherRecipes < Formula
+  desc "ESO SPHERE instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.53.2.tar.gz"
+  sha256 "d82d513061dde8112382708f87ee5b579b2a07a48eb343f28269c9a8abd2f894"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?spher-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cfitsio@4.2.0"
+  depends_on "cpl@7.3.2"
+  depends_on "curl"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl@2.6"
+
+  def install
+    ENV.prepend "LDFLAGS", "-L#{Formula["fftw@3.3.9"].opt_lib}"
+    ENV.prepend "LDFLAGS", "-L#{Formula["wcslib@7.12"].opt_lib}"
+    ENV.prepend "LDFLAGS", "-L#{Formula["cfitsio@4.2.0"].opt_lib}"
+
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "spher-#{version_norevision}.tar.gz"
+    cd "spher-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+             "--with-cfitsio=#{Formula["cfitsio@4.2.0"].prefix}",
+             "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+             "--with-erfa=#{Formula["erfa"].prefix}",
+             "--with-curl=#{Formula["curl"].prefix}",
+             "--with-gsl=#{Formula["gsl@2.6"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "sph_zpl_master_dark -- version", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page sph_zpl_master_dark")
+  end
+end
