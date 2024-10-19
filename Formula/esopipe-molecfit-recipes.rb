c7d68b4fac858bbc536f18c7b5519e062e9cb4aa
szampier <56970291+szampier@users.noreply.github.com>
2024-10-19 17:07:56 +0000

    esopipe-molecfit-recipes: add 4.3.3 bottle.

--- Formula/esopipe-molecfit-recipes.rb
+++ Formula/esopipe-molecfit-recipes.rb
@@ -10,6 +10,13 @@ class EsopipeMolecfitRecipes < Formula
     regex(/href=.*?molecfit-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/esopipe-molecfit-recipes-4.3.3"
+    sha256 cellar: :any,                 arm64_sonoma: "f55fe564bba4e6dbbdc1ac09f5d562695dbd4e179e230e2ddf9283d726151ee7"
+    sha256 cellar: :any,                 ventura:      "190a72e58636bb7be76f17f1e4969a009b5a78831fd60a53d27c5c901c01ac79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e028dfc63a404dcf05a95e4dfd91496ad156ba5fb3d9e176580d2cff86cadcc1"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cpl@7.3.2"
   depends_on "esorex"
