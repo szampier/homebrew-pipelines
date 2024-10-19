f29c919f4aeed5cf3a2178906fbe9744cd05e706
Stefano Zampieri <szampier@eso.org>
2024-10-19 18:48:04 +0200

    add molecfit recipes

---
+++ Formula/esopipe-molecfit-recipes.rb
@@ -0,0 +1,33 @@
+class EsopipeMolecfitRecipes < Formula
+  desc "ESO MOLECFIT instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipelines/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/molecfit/molecfit-kit-4.3.3.tar.gz"
+  sha256 "1b33df7da828d9be81fb54ad5251e236ffa8e53ceaa43c746a08b28ec8e84fc2"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?molecfit-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "esorex"
+  depends_on "telluriccorr"
+
+  def install
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    system "tar", "xf", "molecfit-#{version_norevision}.tar.gz"
+    cd "molecfit-#{version_norevision}" do
+      system "./configure", "--prefix=#{prefix}",
+             "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+             "--with-telluriccorr=#{Formula["telluriccorr"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
+    assert_match "molecfit_dark -- version #{version_norevision}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page molecfit_dark")
+  end
+end
