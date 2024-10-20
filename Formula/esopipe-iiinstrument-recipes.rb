898c40ac0e03648ac8b6307400e24cd4d1186703
Stefano Zampieri <szampier@eso.org>
2024-10-20 11:42:50 +0200

    rename iiinstrument

--- Formula/esopipe-iiinstrument-recipes.rb
+++ Formula/esopipe-iiinstrument-recipes.rb
@@ -1,4 +1,4 @@
-class Iiinstrument < Formula
+class EsopipeIiinstrumentRecipes < Formula
   desc "ESO example template instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/iiinstrument/iiinstrument-kit-0.1.14-50.tar.gz"
