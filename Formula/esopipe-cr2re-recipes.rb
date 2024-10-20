f9ab9a6f6ef824f5b6b123381f93940330e1079e
Stefano Zampieri <szampier@eso.org>
2024-10-20 11:29:57 +0200

    rename cr2res -> cr2re

--- Formula/esopipe-cr2re-recipes.rb
+++ Formula/esopipe-cr2re-recipes.rb
@@ -1,4 +1,4 @@
-class EsopipeCr2resRecipes < Formula
+class EsopipeCr2reRecipes < Formula
   desc "ESO CR2RES instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipelines/"
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/cr2res/cr2re-kit-1.4.4.tar.gz"
@@ -8,13 +8,6 @@ class EsopipeCr2resRecipes < Formula
   livecheck do
     url :homepage
     regex(/href=.*?cr2re-kit-(\d+(?:[.-]\d+)+)\.t/i)
-  end
-
-  bottle do
-    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/esopipe-cr2res-recipes-1.4.4"
-    sha256 cellar: :any,                 arm64_sonoma: "9b80e19b703ff5ea1bc81dbcc74d3e454f39fc8145b86ec110ac0389814f4f79"
-    sha256 cellar: :any,                 ventura:      "07743ca58ac2cb766ef7baff3b5ed7aeb16df4ccf16ccd8306a320a4bcbe7ece"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f081b28a6d41ae778160e7b0a618b89bcf5399b17e56a345cdd97773a8dd32b8"
   end
 
   depends_on "pkg-config" => :build
