f048608b039756c4329d419f444fb21441a65d2b
Stefano Zampieri <szampier@eso.org>
2024-10-27 13:49:32 +0100

    esopipe-spher 0.56.0

--- Formula/esopipe-spher.rb
+++ Formula/esopipe-spher.rb
@@ -1,8 +1,8 @@
 class EsopipeSpher < Formula
   desc "ESO SPHERE instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipelines/"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.53.2.tar.gz"
-  sha256 "d82d513061dde8112382708f87ee5b579b2a07a48eb343f28269c9a8abd2f894"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.56.0.tar.gz"
+  sha256 "0c06cfa743b85fbf2980d0dbdb1bcf8cf57ea591e1f1cec987d6ca4abdc25157"
   license "GPL-2.0-or-later"
 
   livecheck do
